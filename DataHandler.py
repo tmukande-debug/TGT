321397dfa05dd4a197bc06ee1b7102ef9840163e
Tendai Mukande <75079602+tmukande-debug@users.noreply.github.com>
2022-10-19 23:21:47 +0200

    Update DataHandler.py

--- DataHandler.py
+++ DataHandler.py
@@ -55,13 +55,13 @@ def timeProcess(trnMats):
 class DataHandler:
 	def __init__(self):
 		if args.data == 'taobao':
-			predir = 'content/TGT/Datasets/Taobao/'
+			predir = '/content/TGT/Datasets/Taobao/'
 			behs = ['pv', 'fav', 'cart', 'buy']
 		elif args.data == 'ijcai':
-			predir = 'content/TGT/Datasets/ijcai/'
+			predir = '/content/TGT/Datasets/ijcai/'
 			behs = ['click', 'fav', 'cart', 'buy']
 		elif args.data == 'jd':
-			predir = 'content/TGT/Datasets/JD2021/'
+			predir = '/content/TGT/Datasets/JD2021/'
 			behs = ['browse', 'review', 'buy']
 		
 		self.predir = predir
