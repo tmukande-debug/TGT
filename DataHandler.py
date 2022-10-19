04b08af5d0f0ca9e05522c9ca5f36a34039cb14b
Tendai Mukande <75079602+tmukande-debug@users.noreply.github.com>
2022-10-19 23:10:44 +0200

    Update DataHandler.py

--- DataHandler.py
+++ DataHandler.py
@@ -58,7 +58,7 @@ class DataHandler:
 			predir = 'content/TGT/Datasets/Taobao/'
 			behs = ['pv', 'fav', 'cart', 'buy']
 		elif args.data == 'ijcai':
-			predir = 'Content/TGT/Datasets/ijcai/'
+			predir = 'content/TGT/Datasets/ijcai/'
 			behs = ['click', 'fav', 'cart', 'buy']
 		elif args.data == 'jd':
 			predir = 'content/TGT/Datasets/JD2021/'
